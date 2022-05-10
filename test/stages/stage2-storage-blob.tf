4f6b6477a6a0e06de0adc13ae4779ba0aaa670bb
Saurabh Rajguru <saurabh.rajguru@ibm.com>
2022-05-10 16:25:14 +0530

    Validation for the blob file creation
    Validation for the blob file creation
    
    Signed-off-by: Saurabh Rajguru <saurabh.rajguru@ibm.com>

--- test/stages/stage2-storage-blob.tf
+++ test/stages/stage2-storage-blob.tf
@@ -1,7 +1,9 @@
 module "azure-storage-blob" {
-  source               = "./module"
-  resource_group_name  = module.resource_group.name
-  region               = var.region
-  storage_account_name = "stgaccvalidate"
-  container_name       = "containervalidate"
+  source                    = "./module"
+  resource_group_name       = module.resource_group.name
+  region                    = var.region
+  storage_account_name      = "stgaccvalidate"
+  container_name            = "containervalidate"
+  blob_file_name            = "variables.tf"
+  blob_file_sourcefile_name = "variables.tf"
 }
